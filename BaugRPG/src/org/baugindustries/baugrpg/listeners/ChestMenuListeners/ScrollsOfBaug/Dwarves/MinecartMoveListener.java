97346d276949a2370938c07deab04daad920aeab
Hav0k42 <billybobjoebobpi@gmail.com>
2021-10-18 18:15:39 -0400

    Fixed and tested the minecart mechanic for dwarves. Works pretty much perfectly now. It is kind of annoying, because the code to do this gets called every tick and I wish there was a way that doesnt require that. There probably is and I don't know about it.

---
+++ BaugRPG/src/org/baugindustries/baugrpg/listeners/ChestMenuListeners/ScrollsOfBaug/Dwarves/MinecartMoveListener.java
@@ -0,0 +1,32 @@
+package org.baugindustries.baugrpg.listeners.ChestMenuListeners.ScrollsOfBaug.Dwarves;
+
+import org.baugindustries.baugrpg.Main;
+import org.baugindustries.baugrpg.listeners.ChestMenuListeners.ScrollsOfBaug.Elves.CommunismHub.ElvesCommunismHubInventoryListener;
+import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Minecart;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.vehicle.VehicleEnterEvent;
+import org.bukkit.event.vehicle.VehicleMoveEvent;
+import org.bukkit.persistence.PersistentDataType;
+
+public class MinecartMoveListener implements Listener{
+	private Main plugin;
+	public MinecartMoveListener(Main plugin) {
+		this.plugin = plugin;
+	}
+	
+	@EventHandler
+    public void enter(VehicleMoveEvent event) {
+        if (event.getVehicle() instanceof Minecart) {
+            Player player = (Player) event.getVehicle().getPassenger();
+            Minecart minecart = (Minecart) event.getVehicle();
+            
+            if (player.getPersistentDataContainer().get(new NamespacedKey(plugin, "Race"), PersistentDataType.INTEGER) == 3) {//Check if the player is of the race of Dwarf
+            	minecart.setVelocity(minecart.getVelocity().multiply(10));
+            }
+        }
+    }
+}
