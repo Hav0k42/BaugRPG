aba18ad2f2c2f2871878fc20c3de87eafa635fb4
Hav0k42 <billybobjoebobpi@gmail.com>
2022-01-25 02:11:16 -0900

    Added an option to disable learning new recipes in the admin config

---
+++ BaugRPG/src/org/baugindustries/baugrpg/listeners/DisableRecipeListener.java
@@ -0,0 +1,25 @@
+package org.baugindustries.baugrpg.listeners;
+
+import java.io.File;
+
+import org.baugindustries.baugrpg.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerRecipeDiscoverEvent;
+
+public class DisableRecipeListener implements Listener {
+	private Main plugin;
+	public DisableRecipeListener(Main plugin) {
+		this.plugin = plugin;
+	}
+	@EventHandler
+	public void RevokeRecipe(PlayerRecipeDiscoverEvent event) {
+		File file = new File(plugin.getDataFolder() + File.separator + "config.yml");
+	 	FileConfiguration config = YamlConfiguration.loadConfiguration(file);
+	 	if (!(config.contains("allowRecipe") && !config.getBoolean("allowRecipe"))) return;
+		event.setCancelled(true);
+	}
+}
